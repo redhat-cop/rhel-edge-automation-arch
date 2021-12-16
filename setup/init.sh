b6b29d1c6aa0a0aeda4101b5961fb84ef7c18c4c
Andrew Block <andy.block@gmail.com>
2021-12-15 19:07:24 -0600

    Dependency management corrections for init script

--- setup/init.sh
+++ setup/init.sh
@@ -4,14 +4,17 @@ SCRIPT_BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Installs OpenShift GitOps and Deploys ArgoCD
 echo "Deploying OpenShit GitOps..."
-helm upgrade -i -n openshift-operators openshift-gitops "${SCRIPT_BASE_DIR}/../charts/operator" -f "${SCRIPT_BASE_DIR}/values-init.yaml" --dependency-update
+helm dependency update "${SCRIPT_BASE_DIR}/../charts/operator"
+helm upgrade -i -n openshift-operators openshift-gitops "${SCRIPT_BASE_DIR}/../charts/operator" -f "${SCRIPT_BASE_DIR}/values-init.yaml"
 
 echo "Waiting for OpenShift GitOps to Deploy..."
 # Wait for ArgoCD Resource to become established
 until kubectl wait crd/argocds.argoproj.io --for condition=established &>/dev/null; do sleep 5; done
 
 echo "Creating RFE GitOps Namespace"
-helm upgrade -i -n openshift-operators rfe-gitops-namespace "${SCRIPT_BASE_DIR}/../charts/namespaces" -f "${SCRIPT_BASE_DIR}/values-init.yaml" --dependency-update
+helm dependency update "${SCRIPT_BASE_DIR}/../charts/namespaces"
+helm upgrade -i -n openshift-operators rfe-gitops-namespace "${SCRIPT_BASE_DIR}/../charts/namespaces" -f "${SCRIPT_BASE_DIR}/values-init.yaml"
 
 echo "Deploying ArgoCD.."
-helm upgrade -i -n rfe-gitops argocd "${SCRIPT_BASE_DIR}/../charts/argocd" --dependency-update
+helm dependency update "${SCRIPT_BASE_DIR}/../charts/argocd"
+helm upgrade -i -n rfe-gitops argocd "${SCRIPT_BASE_DIR}/../charts/argocd"
