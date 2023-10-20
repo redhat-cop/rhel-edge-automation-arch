cfcadd7761b109b10e17c5927c57e8305ee46ef6
Andrew Block <andy.block@gmail.com>
2023-10-20 16:31:16 -0500

    Migration of ArgoCD to v1beta1
    Signed-off-by: Andrew Block <andy.block@gmail.com>

--- setup/init.sh
+++ setup/init.sh
@@ -2,19 +2,20 @@
 
 SCRIPT_BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Installs OpenShift GitOps and Deploys ArgoCD
+# Configures Initial Namespaces
+echo "Creating Initial Namespaces"
+helm dependency update "${SCRIPT_BASE_DIR}/../charts/namespaces"
+helm upgrade -i -n openshift-operators rfe-gitops-namespace "${SCRIPT_BASE_DIR}/../charts/namespaces" -f "${SCRIPT_BASE_DIR}/values-init.yaml"
+
+# Installs OpenShift GitOps
 echo "Deploying OpenShit GitOps..."
 helm dependency update "${SCRIPT_BASE_DIR}/../charts/operator"
-helm upgrade -i -n openshift-operators openshift-gitops "${SCRIPT_BASE_DIR}/../charts/operator" -f "${SCRIPT_BASE_DIR}/values-init.yaml"
+helm upgrade -i -n openshift-gitops-operator openshift-gitops "${SCRIPT_BASE_DIR}/../charts/operator" -f "${SCRIPT_BASE_DIR}/values-init.yaml"
 
 echo "Waiting for OpenShift GitOps to Deploy..."
 # Wait for ArgoCD Resource to become established
 until kubectl wait crd/argocds.argoproj.io --for condition=established &>/dev/null; do sleep 5; done
 
-echo "Creating RFE GitOps Namespace"
-helm dependency update "${SCRIPT_BASE_DIR}/../charts/namespaces"
-helm upgrade -i -n openshift-operators rfe-gitops-namespace "${SCRIPT_BASE_DIR}/../charts/namespaces" -f "${SCRIPT_BASE_DIR}/values-init.yaml"
-
-echo "Deploying ArgoCD.."
+# echo "Deploying ArgoCD.."
 helm dependency update "${SCRIPT_BASE_DIR}/../charts/argocd"
 helm upgrade -i -n rfe-gitops argocd "${SCRIPT_BASE_DIR}/../charts/argocd"
